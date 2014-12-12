aecfd044315a03c655f390a0b3d240c1408fbd2c
hokutosei <jeanepaul@gmail.com>
2014-12-12 15:51:20 +0000

    update

--- modules/news_getter/googlenews.go
+++ modules/news_getter/googlenews.go
@@ -6,6 +6,21 @@ import(
 	_ "time"
 )
 
+// type jsonNewsBody struct {
+// 	By				string
+// 	Id				int
+// 	//Kids 			[]int
+// 	Score			int
+// 	Text			string
+// 	Time			int
+// 	Title			string
+// 	Type			string
+// 	Url				string
+// 	ProviderName	string
+// 	ProviderUrl		string
+// 	CreatedAt		string
+// }
+
 type GoogleNews map[string]interface {}
 
 type GoogleNewsResponseData struct {
@@ -14,17 +29,27 @@ type GoogleNewsResponseData struct {
 	}
 }
 
+type ResponseData struct {
+	Results []GoogleNewsResults
+}	
+
 type GoogleNewsResults struct {
-	GsearchResultClass string
-	ClusterUrl	string
-	Content string
-	UnescapedUrl	string
-	Url	string
-	Title	string
+	GsearchResultClass 	string
+	ClusterUrl			string
+	Content 			string
+	UnescapedUrl		string
+	Url					string
+	Title				string
 	TitleNoFormatting	string
-	Publish	string
-	PublishedDate	string
-	Language	string
+	Publish				string
+	PublishedDate		string
+	Language			string
+	RelatedStories 		[]RelatedStories
+}
+
+type RelatedStories struct {
+	Url string
+	TitleNoFormatting string
 }
 
 var (
@@ -36,7 +61,9 @@ var (
 
 func StartGoogleNews() {
 	url := fmt.Sprintf("https://ajax.googleapis.com/ajax/services/search/news?v=1.0&topic=t&ned=jp&userip=192.168.0.1")
-	var google_news GoogleNews
+	// var google_news GoogleNews
+	var google_news GoogleNewsResponseData
+	//var testN map[string]interface{}
 	response, _ := httpGet(url)
 	defer response.Body.Close()
 
@@ -45,9 +72,18 @@ func StartGoogleNews() {
 		//return id_containers
 		fmt.Println(err)
 	}
+
+	//fmt.Println(testN)
+
+
+	GNResponse := google_news.ResponseData
+	for _, n := range GNResponse.Results {
+		fmt.Println(n.RelatedStories)
+		fmt.Println("-------------------------")
+	}
 	//r := GoogleNewsResponseData{google_news}
 	//fmt.Println(r.Results)
-	fmt.Println(google_news)
+	//fmt.Println(google_news)
 	//response_data := google_news.ResponseData
 	//fmt.Println(response_data.Results)
 //	for _, s:= range response_data.Results {
