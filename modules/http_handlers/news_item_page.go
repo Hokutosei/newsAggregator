d95aa3b18c15e872354f56877d4be9959fb352e0
hokutosei <jeanepaul@gmail.com>
2015-05-28 13:27:04 +0900

    news item page implementation

---
+++ modules/http_handlers/news_item_page.go
@@ -0,0 +1,28 @@
+package http_handlers
+
+import (
+	_ "encoding/json"
+	"fmt"
+	"net/http"
+
+	"web_apps/news_aggregator/modules/database"
+)
+
+// NewsItemParams struct for news item request
+type NewsItemParams struct {
+	NewsID string `json:"news_id"`
+}
+
+// NewsItemPage http handler for news item page request
+func NewsItemPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("newsitemPage handler!")
+
+	newsID := r.URL.Query().Get("news_id")
+	result, err := database.NewsItemPage(newsID)
+	if err != nil {
+		return
+	}
+
+	respondToJson(w, result)
+	//NewsItemPage
+}
--- public/js/news_page/newspageController.js
+++ public/js/news_page/newspageController.js
@@ -3,13 +3,11 @@
 
     var log = function(str) { console.log(str); };
 
-    app.controller('NewsPageCtrl', function($routeParams, newsPageService) {
-        log($routeParams)
-        log("load news page")
+    app.controller('NewsPageCtrl', function($routeParams, newsPageService, $scope) {
+        newsPageService.getNewsItem($routeParams.id).then(function(resp, status) {
+            $scope.news_item = resp.data
+            log($scope.news_item)
 
-        newsPageService.getNewsItem($routeParams.id).then(function(data, status) {
-            log(data)
-            log(status)
         })
 
     })
