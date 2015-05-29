df323de609ec879dbf43fd1956cd769f89ae8507
hokutosei <jeanepaul@gmail.com>
2015-05-29 17:09:50 +0900

    change news item click to display news item page instead

--- public/js/main.js
+++ public/js/main.js
@@ -64,10 +64,12 @@
         	return new Date(unix_time * 1000)
         };
 
-        $scope.absURL = function(news_item_id) {
+        $scope.absURL = function(news_item_id, index_url) {
             var protocol = $location.protocol()
                 , port = $location.port()
-            return protocol + '://' + location.host + '/news/' + news_item_id + '#disqus_thread'
+                , urlString = protocol + '://' + location.host + '/news/' + news_item_id
+
+            return index_url == true ? urlString : urlString + '#disqus_thread';
         }
     }]);
 }());
