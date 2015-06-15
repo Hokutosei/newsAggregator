003fdf2b3fab77d3181bcf7a8b18300d2932a8ff
jeanepaul <jeanepaul@gmail.com>
2015-06-15 18:08:30 +0900

    testing user location

---
+++ public/js/services/user_location.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  app.factory('userLocation', function() {
+
+    return {
+      getLocation: function() {
+        console.log(navigator.geolocation)
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+              console.log(position)
+              // $scope.$apply(function(){
+              //   $scope.position = position;
+              // });
+            });
+          }
+      }
+    }
+
+  })
+
+}())
