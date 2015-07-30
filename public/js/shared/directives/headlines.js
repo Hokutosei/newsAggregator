32e03813587f958875bb6c7773585e8164c08d09
jeanepaul <jeanepaul@gmail.com>
2015-07-31 07:55:27 +0900

    headline UI implementation

--- public/js/shared/directives/headlines.js
+++ public/js/shared/directives/headlines.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  app.directive('headlines', function(httpService) {
+  app.directive('headlines', function(httpService, $location) {
     return {
       restrict: 'E',
       templateUrl: 'js/shared/templates/headlines.html',
@@ -9,6 +9,15 @@
         httpService.fetchHeadlines(function(data) {
           scope.headlines = data;
         })
+        console.log($location.path())
+        scope.absURL = function(news_item_id, index_url) {
+            var protocol = $location.protocol()
+                , port = $location.port()
+                , urlString = protocol + '://' + location.host + '/news/' + news_item_id
+
+            return index_url == true ? urlString : urlString + '#disqus_thread';
+        }
+
       }
     }
   })
